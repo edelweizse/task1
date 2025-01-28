48f729eef37c87d7fd960a84d27e0f21d9eee0a1
edelweizse <231254@astanait.edu.kz>
2025-01-28 10:31:02 +0500

    231

--- src/app/page.js
+++ src/app/page.js
@@ -28,16 +28,16 @@ const TypingText = ({ text, speed = 100 }) => {
 
 export default function Home() {
   return (
-    <div className="max-w-4xl mx-auto p-4 text-green-600">
+    <div className="max-w-4xl mx-auto p-4 text-gr">
       <header className="text-center my-8">
-        <h1 className="text-4xl font-bold text-green-600">
-          <TypingText text="Linux Klub Cicada3301" speed={75}/>
+        <h1 className="text-4xl font-bold text-gr">
+          <TypingText text="[Linux Klub]" speed={75}/>
         </h1>
       </header>
-      <section className="rounded-2xl shadow-lg mb-8 border border-green-600">
-        <div className="flex flex-col items-center justify-center text-green-600 p-5">
+      <section className="shadow-lg mb-8 border-2 border-gr ">
+        <div className="flex flex-col items-center justify-center text-gr p-5">
           <h2 className="text-2xl font-semibold text-center">
-            <TypingText text="Look beyond the visible"/>
+            <TypingText text="Task 1: Look beyond the visible" speed={50}/>
           </h2>
           <div className="w-full max-w-[500px] mt-4">
           <a 
@@ -46,7 +46,7 @@ export default function Home() {
             className="block w-full max-w-[500px] cursor-pointer"
           >
             <div 
-              className="quiz_image w-full aspect-[5/2] rounded-lg border border-green-600 bg-green-700 hover:bg-green-800 transition-colors duration-300"
+              className="quiz_image w-full aspect-[5/2] border border-green-600 bg-green-700 hover:bg-green-800 transition-colors duration-300"
               role="img"
               aria-label="Downloadable quiz image"
             />
@@ -55,14 +55,21 @@ export default function Home() {
           </div>
       </section>
       <section>
-        <p className="text-green-400 mb-4">
+        <p className="text-gr mb-2">
           Rules:
         </p>
-        <p className="text-green-400">
+        <ul>
+          <li>Rule 1:</li>
+          <li>Rule 2:</li>
+          <li>Rule 3:</li>
+        </ul>
+        <p className="text-gr mt-4">
           Brought to you by <span className="font-bold">Linux Klub</span>, a community for tech enthusiasts. Dive in and see how many you can get right!
         </p>
     </section>
-
+    <p className="hidden">
+      3301
+    </p>
     </div>
   );
 }
